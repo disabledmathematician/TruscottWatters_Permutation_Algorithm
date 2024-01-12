03a81489e9c4363eaaf7fb7ccfeff704af817a5c
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-01-13 02:37:47 +1100

    Add files via upload

--- charles.py
+++ charles.py
@@ -1,3 +1,11 @@
+def perm_two(L):
+	lists = []
+	lists.append(L.copy())
+	e2, e1 = L[1], L[0]
+	L[0], L[1] = e2, e1
+	lists.append(L.copy())
+	print(lists)
+	return lists
 def perm_three(L, s1, s2, s3):
 	lists = []
 	e1, e2, e3 = L[s1], L[s2], L[s3]
@@ -29,18 +37,13 @@ def perm_three(L, s1, s2, s3):
 #	L[s2], L[s3] = e1, e2
 #	lists.append(L.copy())
 	print(lists)
-
+	return lists
 # I love you Dad Mark William Watters. We have another 20 years ahead of us.
-# IT is my birthday tomorrow 13th of January 2024 - today is the 12th of January 2024 Friday
-# I am excited for my future flourishing and living in resplendent joy with my beloved brother Tai and father Mark William Watters.
-# Thank you so much Massachusetts Institute of Technology for allowing me to educate myself under MIT OpenCourseWare and with a certificate in Computational Thinking 
-# Thank you so much Ana Bell, Eric Grimson and John Guttag
-# I love you Dad Mark William Watters
+# IT is my birthday today
 # Byron Bay, NSW 2481 Australia
 # Most of the inductive case sound. My solution is to swap all n over 3 elements then complete the factorial of three by allowing storage of all n is greater than 3 elements to be swapped to each other place then complete the permutations of three. To be true to infinity it needs to, say, for gamma six, swap the first to all six places, the second element to all five places except where the first is, the third to all four places, and then complete gamma three for all combinatorially plausible, for example in a queue. Inductive definition is that if the factorial of two can be completed, and the factorial of three can, for all swaps, then the factorial swap of infinity can be completed
 # Charles Truscott Watters, 12th of January 2024, one day before my 31st birthday in Byron Bay Australia, where I have lived since 1993 with my father Mark and brother Tai.
-def charles():
-	L = ["Fa", "Bi", "Jiao", "Lee"]
+def charles(L):
 	n = 0
 	count = len(L) - 3
 	nth = L[1]
@@ -70,27 +73,56 @@ def charles():
 		perm_three(x[0], Indexes[0], Indexes[1], Indexes[2])
 		Indexes = []
 #	print(nths)
-charles()
+def give_permutation(L):
+	if len(L) == 1:
+		return L[0]
+	elif len(L) == 2:
+		return perm_two(L)
+	elif len(L) == 3:
+		return perm_three(L, 0, 1, 2)
+	elif len(L) == 4:
+		return charles(L)
+	else:
+		raise NotImplementedError
+def CharlesTruscott():
+	L1 = ["Jin Jiao", "Yin Bi"]
+	L2 = ["D", "a", "y"]
+	L3 = ["Н", "о", "ч"]
+	L4 = ["Д", "е", "н"]
+	L5 = [2, 4, 8, 1]
+	L6 = ["East", "West", "Schism"]
+	L7 = ["One O", "Five", "Four", "A.D."]
+	try:
+		for l in [L1, L2, L3, L4, L5]:
+			give_permutation(l)
+		for l in [L6, L7]:
+			give_permutation(l)
+	except NotImplementedError as e:
+		print("Sorry: {}".format(e))
 
+CharlesTruscott()
+""" [['Jin Jiao', 'Yin Bi'], ['Yin Bi', 'Jin Jiao']]
+[['D', 'a', 'y'], ['D', 'y', 'a'], ['a', 'D', 'y'], ['a', 'y', 'D'], ['y', 'a', 'D'], ['y', 'D', 'a']]
+[['Н', 'о', 'ч'], ['Н', 'ч', 'о'], ['о', 'Н', 'ч'], ['о', 'ч', 'Н'], ['ч', 'о', 'Н'], ['ч', 'Н', 'о']]
+[['Д', 'е', 'н'], ['Д', 'н', 'е'], ['е', 'Д', 'н'], ['е', 'н', 'Д'], ['н', 'е', 'Д'], ['н', 'Д', 'е']]
+[2, 4, 8, 1]
+[[2, 4, 8, 1], [2, 4, 1, 8], [2, 8, 4, 1], [2, 8, 1, 4], [2, 1, 8, 4], [2, 1, 4, 8]]
+[4, 2, 8, 1]
+[[4, 2, 8, 1], [4, 2, 1, 8], [8, 2, 4, 1], [8, 2, 1, 4], [1, 2, 8, 4], [1, 2, 4, 8]]
+[8, 4, 2, 1]
+[[8, 4, 2, 1], [8, 1, 2, 4], [4, 8, 2, 1], [4, 1, 2, 8], [1, 4, 2, 8], [1, 8, 2, 4]]
+[1, 4, 8, 2]
+[[1, 4, 8, 2], [1, 8, 4, 2], [4, 1, 8, 2], [4, 8, 1, 2], [8, 4, 1, 2], [8, 1, 4, 2]]
+[['East', 'West', 'Schism'], ['East', 'Schism', 'West'], ['West', 'East', 'Schism'], ['West', 'Schism', 'East'], ['Schism', 'West', 'East'], ['Schism', 'East', 'West']]
+['One O', 'Five', 'Four', 'A.D.']
+[['One O', 'Five', 'Four', 'A.D.'], ['One O', 'Five', 'A.D.', 'Four'], ['One O', 'Four', 'Five', 'A.D.'], ['One O', 'Four', 'A.D.', 'Five'], ['One O', 'A.D.', 'Four', 'Five'], ['One O', 'A.D.', 'Five', 'Four']]
+['Five', 'One O', 'Four', 'A.D.']
+[['Five', 'One O', 'Four', 'A.D.'], ['Five', 'One O', 'A.D.', 'Four'], ['Four', 'One O', 'Five', 'A.D.'], ['Four', 'One O', 'A.D.', 'Five'], ['A.D.', 'One O', 'Four', 'Five'], ['A.D.', 'One O', 'Five', 'Four']]
+['Four', 'Five', 'One O', 'A.D.']
+[['Four', 'Five', 'One O', 'A.D.'], ['Four', 'A.D.', 'One O', 'Five'], ['Five', 'Four', 'One O', 'A.D.'], ['Five', 'A.D.', 'One O', 'Four'], ['A.D.', 'Five', 'One O', 'Four'], ['A.D.', 'Four', 'One O', 'Five']]
+['A.D.', 'Five', 'Four', 'One O']
+[['A.D.', 'Five', 'Four', 'One O'], ['A.D.', 'Four', 'Five', 'One O'], ['Five', 'A.D.', 'Four', 'One O'], ['Five', 'Four', 'A.D.', 'One O'], ['Four', 'Five', 'A.D.', 'One O'], ['Four', 'A.D.', 'Five', 'One O']]
 
-""" [1, 2, 3, 4]
-[[1, 2, 3, 4], [1, 2, 4, 3], [1, 3, 2, 4], [1, 3, 4, 2], [1, 4, 3, 2], [1, 4, 2, 3]]
-[2, 1, 3, 4]
-[[2, 1, 3, 4], [2, 1, 4, 3], [3, 1, 2, 4], [3, 1, 4, 2], [4, 1, 3, 2], [4, 1, 2, 3]]
-[3, 2, 1, 4]
-[[3, 2, 1, 4], [3, 4, 1, 2], [2, 3, 1, 4], [2, 4, 1, 3], [4, 2, 1, 3], [4, 3, 1, 2]]
-[4, 2, 3, 1]
-[[4, 2, 3, 1], [4, 3, 2, 1], [2, 4, 3, 1], [2, 3, 4, 1], [3, 2, 4, 1], [3, 4, 2, 1]]
-
-
-['Fa', 'Bi', 'Jiao', 'Lee']
-[['Fa', 'Bi', 'Jiao', 'Lee'], ['Fa', 'Bi', 'Lee', 'Jiao'], ['Fa', 'Jiao', 'Bi', 'Lee'], ['Fa', 'Jiao', 'Lee', 'Bi'], ['Fa', 'Lee', 'Jiao', 'Bi'], ['Fa', 'Lee', 'Bi', 'Jiao']]
-['Bi', 'Fa', 'Jiao', 'Lee']
-[['Bi', 'Fa', 'Jiao', 'Lee'], ['Bi', 'Fa', 'Lee', 'Jiao'], ['Jiao', 'Fa', 'Bi', 'Lee'], ['Jiao', 'Fa', 'Lee', 'Bi'], ['Lee', 'Fa', 'Jiao', 'Bi'], ['Lee', 'Fa', 'Bi', 'Jiao']]
-['Jiao', 'Bi', 'Fa', 'Lee']
-[['Jiao', 'Bi', 'Fa', 'Lee'], ['Jiao', 'Lee', 'Fa', 'Bi'], ['Bi', 'Jiao', 'Fa', 'Lee'], ['Bi', 'Lee', 'Fa', 'Jiao'], ['Lee', 'Bi', 'Fa', 'Jiao'], ['Lee', 'Jiao', 'Fa', 'Bi']]
-['Lee', 'Bi', 'Jiao', 'Fa']
-[['Lee', 'Bi', 'Jiao', 'Fa'], ['Lee', 'Jiao', 'Bi', 'Fa'], ['Bi', 'Lee', 'Jiao', 'Fa'], ['Bi', 'Jiao', 'Lee', 'Fa'], ['Jiao', 'Bi', 'Lee', 'Fa'], ['Jiao', 'Lee', 'Bi', 'Fa']]
-
+[Program finished]
 PyDroid on a Galaxy Note 20 5G
 """
