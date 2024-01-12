92d8b86094dc5c065dbf8977471685d60e58c450
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-01-12 21:41:24 +1100

    Update Charles.py
    Now to work this iteratively or recursively in an abstract data type, maybe even a queue or heap until correct to an infinite input size. Far queue

--- Charles.py
+++ Charles.py
@@ -1,20 +1,35 @@
 def perm_three(L, s1, s2, s3):
 	lists = []
 	e1, e2, e3 = L[s1], L[s2], L[s3]
+	lists.append(L.copy())
 	L[s2], L[s3] = e3, e2
 	lists.append(L.copy())
 	L[s2], L[s3] = e2, e3
 	L[s1], L[s2] = e2, e1
 	lists.append(L.copy())
-	L[s2], L[s3] = e3, e2
+	L[s2], L[s3] = e3, e1
 	lists.append(L.copy())
-	L[s2], L[s3] = e2, e3
-	L[s1], L[s2] = e1, e2
+	L[s1], L[s2], L[s3] = e1, e2, e3
 	L[s1], L[s3] = e3, e1
 	lists.append(L.copy())
 	L[s2], L[s3] = e1, e2
 	lists.append(L.copy())
+#	L[s2], L[s3] = e3, e2
+#	lists.append(L.copy())
+#	L[s2], L[s3] = e2, e3
+#	L[s1], L[s2] = e2, e1
+#	lists.append(L.copy())
+#	L[s1], L[s2] = e1, e2
+#	L[s2], L[s3] = e3, e2
+#	lists.append(L.copy())
+#	L[s2], L[s3] = e2, e3
+#	L[s1], L[s2] = e1, e2
+#	L[s1], L[s3] = e3, e1
+#	lists.append(L.copy())
+#	L[s2], L[s3] = e1, e2
+#	lists.append(L.copy())
 	print(lists)
+
 # I love you Dad Mark William Watters. We have another 20 years ahead of us.
 # IT is my birthday tomorrow 13th of January 2024 - today is the 12th of January 2024 Friday
 # I am excited for my future flourishing and living in resplendent joy with my beloved brother Tai and father Mark William Watters.
@@ -31,9 +46,9 @@ def charles():
 	for x in range(0, len(L)):
 		L[n], L[x] = L[x], L[n]
 		lists.append((L.copy(), x))
-		print(L)
+#		print(L)
 		L[n], L[x] = L[x], L[n]
-	print(lists)
+#	print(lists)
 	nths = []
 	count = 0
 	for x in lists:
@@ -49,7 +64,7 @@ def charles():
 			if n != x[1]:
 #				print(x[0][n])
 				Indexes.append(n)
-		print(Indexes)
+#		print(Indexes)
 		perm_three(x[0], Indexes[0], Indexes[1], Indexes[2])
 		Indexes = []
 	print(nths)
